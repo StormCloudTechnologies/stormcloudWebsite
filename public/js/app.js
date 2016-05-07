10413301e2b3163643af4769542ff793418e555e
irshadgo <irshad.gouri@hotwaxsystems.com>
2016-05-07 13:10:22 +0530

    Added files.

---
+++ public/js/app.js
@@ -0,0 +1,60 @@
+var url_prifix = 'http://52.36.75.89:3000/';
+
+angular.module('AppModule', [])
+
+.controller('AppCtrl', function($scope, APIService) {
+    $scope.sendEmail = function(mail) {
+        if(mail && (mail.name != null || mail.name != '' || mail.name != undefined) && (mail.email != null || mail.email != '' || mail.email != undefined) && (mail.phone != null || mail.phone != '' || mail.phone != undefined) && (mail.message != null || mail.message != '' || mail.message != undefined))
+        {
+            APIService.setData({
+                req_url: url_prifix + 'api/sendMail',
+                data: mail
+            }).then(function(resp) {
+                $scope.successMessage = resp.data.message;
+               },function(resp) {
+                  // This block execute in case of error.
+            });
+        }
+    };
+})
+
+.factory('APIService', ['$http', function($http) {
+    return {
+        getData: function(obj){
+            var xhr = $http({
+                url: obj.req_url,
+                method: 'GET',
+                data: obj.data,
+                headers: {'Content-Type': 'application/json'}
+            });
+            return xhr;
+        },
+        setData: function(obj){
+            var xhr = $http({
+                url: obj.req_url,
+                method: 'POST',
+                data: obj.data,
+                headers: {'Content-Type': 'application/json'}
+            });
+            return xhr;
+        },
+        removeData: function(obj){
+            var xhr = $http({
+                url: obj.req_url,
+                method: 'DELETE',
+                data: obj.data,
+                headers: {'Content-Type': 'application/json'}
+            });
+            return xhr;
+        },
+        updateData: function(obj){
+            var xhr = $http({
+                url: obj.req_url,
+                method: 'PUT',
+                data: obj.data,
+                headers: {'Content-Type': 'application/json'}
+            });
+            return xhr;
+        }
+    };
+}]);
---
+++ server.js
@@ -0,0 +1,59 @@
+// server.js
+
+// set up ========================
+var express  = require('express');
+var app      = express();                               // create our app w/ express
+var morgan = require('morgan');             // log requests to the console (express4)
+var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
+var nodemailer = require('nodemailer');
+
+app.use(function(req, res, next) { //allow cross origin requests
+    res.setHeader("Access-Control-Allow-Methods", "POST, PUT, OPTIONS, DELETE, GET");
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+// configuration
+app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
+app.use(morgan('dev'));                                         // log every request to the console
+app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
+app.use(bodyParser.json());                                     // parse application/json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+
+
+
+// listen (start app with node server.js) ======================================
+app.listen(3000);
+console.log("App listening on port 3000");
+
+var transport = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+      user: "stormcloudtechno@gmail.com",
+      pass: "Gostormcloud89"
+    }
+});
+
+app.get('/', function(req, res) {
+    res.sendfile('./public/index.html'); // load our public/index.html file
+});
+
+// post ad
+app.post('/api/sendMail', function(req, res) {
+        // send mail
+      var msg = {
+        html: "<b>Hello,</b><p><strong>Name: </strong>" + req.body.name + "</p><p><strong>Email: </strong>" + req.body.email + "</p><p><strong>Phone: </strong>" + req.body.phone + "</p><p><strong>Message: </strong>" + req.body.message + "</p>",
+        createTextFromHtml: true,
+        from: "<stormcloudtechno@gmail.com>",
+        to: "<stormcloudtechno@gmail.com>",
+        subject: "Message from website"
+      };
+      transport.sendMail(msg, function (err) {
+        if (err) {
+          console.log("====err=======",err);
+          return;
+        }
+        return res.json({"message":"Your message sent successfully."});
+      });
+});
